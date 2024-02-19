1b3ce3e5a6026d9b3986b8c0e45a83f579ea370a
Abukhanifa <a_kuatuly@kbtu.kz>
2024-02-19 19:23:39 +0600

    added Lab 4

---
+++ Lab 4/src/app/products.ts
@@ -0,0 +1,106 @@
+export interface Product {
+  id: number;
+  name: string;
+  price: number;
+  description: string;
+}
+
+export const products = [
+  {
+    id: 1,
+    name: 'Apple iPhone 13 128Gb, Green',
+    price: 629,
+    rating: '5/5',
+    image: 'https://m.media-amazon.com/images/I/61-r9zOKBCL._AC_SL1500_.jpg',
+    link: 'https://kaspi.kz/shop/p/apple-iphone-13-128gb-zelenyi-104078887/?c=750000000',
+    description: 'The iPhone 13 display has rounded corners that follow a beautiful curved design, and these corners are within a standard rectangle. When measured as a standard rectangular shape, the screen is 6.06 inches diagonally (actual viewable area is less).'
+  },
+  {
+    id: 2,
+    name: 'Xiaomi Redmi 12, 4G 8 Gb/256 Gb, Black',
+    price: 199,
+    rating: '4.6/5',
+    image: 'https://m.media-amazon.com/images/I/61vzO5p1bHL._AC_SL1387_.jpg',
+    link: 'https://kaspi.kz/shop/p/xiaomi-redmi-12-4g-8-gb-256-gb-chernyi-110918152/?c=750000000',
+    description: 'Redmi 12 delivers an amazing balance of power and efficiency thanks to its MediaTek Helio G88 processor, delivering smooth and stable performance day in, day out. Redmi 12 comes with a maximum capacity of 256GB, standing out from the competition.'
+  },
+  {
+    id: 3,
+    name: 'Poco Poco X5 Pro 8 Gb/256 Gb, Black',
+    price: 309,
+    rating: '4.1/5',
+    image: 'https://m.media-amazon.com/images/I/61lQIQNvaeL._AC_UF1000,1000_QL80_.jpg',
+    link: 'https://kaspi.kz/shop/p/poco-poco-x5-pro-8-gb-256-gb-chernyi-108781715/?c=750000000',
+    description: 'With the POCO X5 Pro 5Gs abundance of exceptional features, enjoy smooth operations and great efficiency. The Snapdragon 778G processor in this smartphone is exceptionally fast and well-optimised. Additionally, it gives you dependable functionality whenever you need it thanks to its astounding 545K AnTuTu score. For an immersive experience, this phone also has a premium-grade AMOLED display and an exceptionally slim bezel. Moreover, this phones fantastic 108 MP main camera allows you to take stunning photos and enjoy wonderful image processing. This phones camera also supports 4K video recording at 30 frames per second.'
+  },
+  {
+    id: 4,
+    name: 'Apple iPhone 14 128Gb, Blue',
+    price: 799,
+    rating: '4.7/5',
+    image: 'https://m.media-amazon.com/images/I/61ufE2FHl7L._AC_SL1500_.jpg',
+    link: 'https://kaspi.kz/shop/p/xiaomi-redmi-12-4g-8-gb-256-gb-chernyi-110918152/?c=750000000',
+    description: 'iPhone 14. With the most impressive dual-camera system on iPhone. Capture stunning photos in low light and bright light. Crash Detection, a vital safety feature, can detect a severe car crash and call for help.'
+  },
+  {
+    id: 5,
+    name: 'Huawei nova 11i 8 Gb/128 Gb, Green',
+    price: 629,
+    rating: '4.2/5',
+    image: 'https://m.media-amazon.com/images/I/61VXYlUt-cL._AC_SL1500_.jpg',
+    link: 'https://kaspi.kz/shop/p/huawei-nova-11i-8-gb-128-gb-zelenyi-podarok-110551017/?c=750000000',
+    description: 'The phone boasts a 6.8-inch FullView Display and 1 mm ultra-narrow bezels that push the screen-to-body ratio to an astonishing 94.9%. Let your true colours shine through with all the beauties around echoing in harmony, regardless of day or night.'
+  },
+  {
+    id: 6,
+    name: 'Samsung Galaxy A34 5G 6 Gb/128 Gb, Purple',
+    price: 299,
+    rating: '4.8/5',
+    image: 'https://resources.cdn-kaspi.kz/img/m/p/h7a/hec/80075305386014.jpg?format=gallery-large',
+    link: 'https://kaspi.kz/shop/p/samsung-galaxy-a34-5g-6-gb-128-gb-fioletovyi-109880728/?c=750000000',
+    description: "Galaxy A34 5G's 1,000nits, Super AMOLED display ensures a clear view even when outside. Enjoy silky smooth viewing on the 6.6-inch awesome screen and refresh rates of up to 120Hz that breathe life into your content."
+  },
+  {
+    id: 7,
+    name: 'TECNO Spark Go 2024 4 ГБ/128 ГБ ',
+    price: 399,
+    rating: '4.6/5',
+    image: 'https://resources.cdn-kaspi.kz/img/m/p/h24/h45/84707842981918.png?format=gallery-large',
+    link: 'https://kaspi.kz/shop/p/tecno-spark-go-2024-4-gb-128-gb-belyi-115228761/?c=750000000',
+    description: 'The TECNO Spark Go 2024. It’s the refreshed version of its 2023 predecessor, now sporting a better chipset, a revamped design, and ‘Dynamic Island’-inspired feature. The new model equips the UNISOC T606 chipset packing improved performance (compared to its 2023 version with MediaTek Helio A22).'
+  },
+  {
+    id: 8,
+    name: 'Realme Note 50 4 Gb/128 Gb, Black',
+    price: 259,
+    rating: '4.3/5',
+    image: 'https://resources.cdn-kaspi.kz/img/m/p/hd6/hcf/84991826722846.png?format=gallery-large',
+    link: 'https://kaspi.kz/shop/p/realme-note-50-4-gb-128-gb-chernyi-116138550/?c=750000000',
+    description: 'The Realme Note 50 will come with a 6.74-inch IPS LCD screen with a waterdrop-style notch. As usual in the low-end segment, the display’s bezels are quite prominent with a large chin at the bottom. The power button and the volume rockers are on the right side of the smartphone. The left side serves as a home for a SIM card/microSD card tray.'
+  },
+  {
+    id: 9,
+    name: 'OPPO A98 5G 8 Gb/256 Gb, Black',
+    price: 349,
+    rating: '4.5/5',
+    image: 'https://resources.cdn-kaspi.kz/img/m/p/h87/hce/82803753091102.png?format=gallery-large',
+    link: 'https://kaspi.kz/shop/p/oppo-a98-5g-8-gb-256-gb-chernyi-112526102/?c=750000000',
+    description: 'OPPO A98 5G is a virtuoso, boasting high screen resolution, high pixel density, and high colour saturation for a clear, accurate display. In direct sunlight, screen brightness reaches as high as 680 nits outdoors, so you can even surf and stream in very bright light.'
+  },
+  {
+    id: 10,
+    name: 'Vivo Y17s 4 Gb/128 Gb, Green',
+    price: 199,
+    rating: '4.7/5',
+    image: 'https://resources.cdn-kaspi.kz/img/m/p/hc7/h35/84523523604510.jpg?format=gallery-large',
+    link: 'https://kaspi.kz/shop/p/vivo-y17s-4-gb-128-gb-zelenyi-114694044/?c=750000000',
+    description: 'The vivo Y17s is an upgraded version of the Y11, a high-performance gaming phone with 128GB of storage, expandable up to 1TB. and comes with a Memory Booster with (4GB/6GB) + 4GB of Expanded Memory (Expanded Memory 3.0) for smooth multitasking. It also offers Ultra Game Mode + Multi-Turbo 5.5 for a superb gaming experience.'
+  }
+];
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
